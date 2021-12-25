219706811dd295f72e61886d97446f74c151c5a8
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2021-12-25 06:31:56 +0100

    Bump version

--- clarity-ad-blocker.php
+++ clarity-ad-blocker.php
@@ -5,7 +5,7 @@
  * GitHub Plugin URI: khromov/clarity
  * Description: Remove nags and upsells from popular WordPress plugins.
  * Author:      khromov
- * Version:     1.0.210415
+ * Version:     1.0.211225
  * Requires at least: 5.0
  * Tested up to: 5.6
  * Requires PHP: 7.0
